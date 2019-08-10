6bd5aeb634a21d22be95f3e1640ed08fa7896667
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-10 18:06:26 -0500

    give the ability to log in to the app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
-  def new 
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash.now[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with that information"
+      render 'new'
+    end
   end
 end
