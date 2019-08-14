67861e350853ad202077ff35e322ddbb76fcf71e
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-14 12:52:37 -0500

    add the ability for a logged in user to add a message from the UI, no web sockets yet

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,18 @@
 class MessagesController < ApplicationController
   before_action :require_user
-  
+
   def index
     @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    message = current_user.messages.build(message_params)
+    redirect_to root_path if message.save
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
