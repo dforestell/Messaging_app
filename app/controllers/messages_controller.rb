b1708ad7fb4b489ee13e2115a2c9290a5f4c13f0
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-17 12:26:36 -0500

    add a scope on the messages model so that it can restrict loading to the latest 20 messages to help with performance if we were to get lots of messages in the DB

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :require_user
 
   def index
-    @messages = Message.all
+    @messages = Message.displayed_messages
     @message = Message.new
   end
 
